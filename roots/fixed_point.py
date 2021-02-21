89df489a44e87bafb44173c164804dbacd237502
Felipe Marra <felipeferreiramarra@gmail.com>
2021-02-21 18:40:59 -0300

    .

--- roots/fixed_point.py
+++ roots/fixed_point.py
@@ -1,16 +1,16 @@
-# problem: find root of f (x) = x² − x − 2 = 0 using the fixed point strategy
-#   I = [1.5, 2.5]
+# problem: find root of f (x) = x² − x − 2 = 0 using the fixed point method
+# x in range I = [1.5, 2.5]
 import math
 
 EPSOLON = 0.1
-MAX_ITER = 10000
+MAX_ITER = 10
 X_ZERO = 1.5
 
 
-def y(x): return math.pow(x, 2)-x-2
+def y(x): return x ** 2 - x - 2
 
 
-def fi(x): return 1/2*math.sqrt(x + 2)
+def fi(x): return 1 / 2 * math.sqrt(x + 2)
 
 
 def stop_criteria(x, k):
@@ -24,11 +24,12 @@ def fixedPoint():
     print("X_ZERO = " + str(x))
     while not stop_criteria(x, k):
         x = fi(x)
-        k = k+1
+        k = k + 1
         print("current (x,y) = (" + str(x) + "," + str(y(x)) + ")")
 
 
 fixedPoint()
 
-# como me manter dentro do intervalo I ?
+#ex slide 21
+#como manter x no intervalo I ?
 # método não converge... X_ZERO errado ?
---
+++ roots/newtons_method.py
@@ -0,0 +1,35 @@
+# problem: find root of f (x) = x − x^1/3 − 2 = 0 using the newtons method
+
+EPSOLON = 0
+MAX_ITER = 10
+X_ZERO = 3
+
+
+def stop_criteria(x, k):
+    print("iteration: " + str(k) + "  " + str(abs(f(x))))
+    return abs(f(x)) <= EPSOLON or k >= MAX_ITER
+
+
+def f(x): return x - x ** 1/3 - 2
+
+
+def f_line(x): return 1 - x/3 ** - 2/3
+
+
+def nextX(x): return x - f(x) / f_line(x)
+
+
+def newtons_method():
+    k = 1
+    x = X_ZERO
+
+    while not stop_criteria(x, k):
+        x = nextX(x)
+        k = k+1
+        print("current (x,y) = (" + str(x) + "," + str(f(x)) + ")")
+
+
+newtons_method()
+
+#ex slide 38
+#f(x) == 0 na primeira iteração... número muito pequeno?
