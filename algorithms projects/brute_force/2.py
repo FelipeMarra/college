3aaa2339bd92b445558aba262501e50bdc4c6ebe
Felipe Marra <felipeferreiramarra@gmail.com>
2021-02-23 16:41:32 -0300

    dumb way

--- algorithms projects/brute_force/2.py
+++ algorithms projects/brute_force/2.py
@@ -4,4 +4,29 @@
 #esse problema, o vetor não pode ser ordenado. Implemente e teste seu algoritmo 
 # em alguma linguagem de programação.
 ##############################################################################
+import math
 
+def find_biggest_elements(v,biggest_elements):
+    for elem in v:
+        if(elem > biggest_elements[0]):
+            biggest_elements[0] = elem
+    for elem in v:
+        if(elem < biggest_elements[0] and elem > biggest_elements[1]):
+            biggest_elements[1] = elem
+
+
+
+def main():
+    v = []
+    #coletar vetor
+    print("digite o tamanho do vetor ")
+    tam = int(input())
+    for i in range(tam):
+        print("digite o valor " + str(i))
+        value = float(input())
+        v.append(value)
+    biggest_elements = [v[0],v[1]]
+    find_biggest_elements(v,biggest_elements)
+    print(biggest_elements)
+
+main()
