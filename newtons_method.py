a6de7dbb062fb43415db785197da4123cc8bb998
Felipe Marra <felipeferreiramarra@gmail.com>
2021-02-24 19:03:45 -0300

    .

---
+++ newtons_method.py
@@ -0,0 +1,25 @@
+class NewtonsMethod:
+    def __init__(self, EPSILON, MAX_ITER, X_ZERO):
+        self.EPSILON = EPSILON
+        self.MAX_ITER = MAX_ITER
+        self.X_ZERO = X_ZERO
+
+    def stop_criteria(self, f, x, k):
+        print("iteration: " + str(k))
+        return abs(f(x)) <= self.EPSILON or k >= self.MAX_ITER 
+
+    def nextX(self, f, f_prime, x): return x - f(x) / f_prime(x)
+
+    def custom_print(self, f, x):
+        return print("current (x,y) = (" + str(x) + "," + str(f(x)) + ")")
+
+    def calculate(self, f, f_prime):
+        k = 1
+        x = self.X_ZERO
+        print("X_ZERO = " + str(x))
+        while not self.stop_criteria(f, x, k):
+            x = self.nextX(f, f_prime, x)
+            k = k+1
+            self.custom_print(f, x)
+        self.custom_print(f,x)
+        return x
---
+++ problem.py
@@ -0,0 +1,59 @@
+from newtons_method import NewtonsMethod
+
+# algorithm constants
+EPSILON = 10 ** -6
+MAX_ITER = 10
+
+# equasion constants
+RZAO = 0.082
+A_ZERO = 2.2769
+B_ZERO = 0.05587
+AZINHO = 0.01855
+BZINHO = -0.01587
+CZINHO = 12.83 * 10 ** 4
+
+
+def initial_v(t, p):
+    return RZAO*t/p
+
+
+def beta(t):
+    return (RZAO * t * B_ZERO) - A_ZERO - (RZAO * CZINHO/t**2)
+
+
+def gamma(t):
+    return (-RZAO * t * B_ZERO * BZINHO) + (A_ZERO * AZINHO) - (RZAO*CZINHO*B_ZERO/t**2)
+
+
+def delta(t):
+    return (RZAO * B_ZERO * BZINHO * CZINHO) / t ** 2
+
+
+def get_volume(p, t, v, beta, gamma, delta):
+    return (RZAO * t * v ** -1) + (beta * v ** -2) + (gamma * v ** -3) + (delta * v ** -4) - p
+
+
+def get_volume_prime(p, t, v, beta, gamma, delta):
+    return ((RZAO * t) * (-v ** -2)) + (beta * (-2 * v ** -3)) + (gamma * (-3 * v ** -4)) + (delta * (-4 * v ** -5))
+
+
+def main():
+    temperatures = [0,200]
+    #convert to kelvin
+    temperatures = [t+273 for t in temperatures]
+    atm = [1, 2, 5, 20, 40, 60, 80, 120, 140, 160, 180, 200]
+
+    for t in temperatures:
+        print("PARA T = " + str(t))
+        beta_value = beta(t)
+        gamma_value = gamma(t)
+        delta_value = delta(t)
+        for p in atm:
+            print("PARA PRESSAO = " + str(p))
+            newton = NewtonsMethod(EPSILON, MAX_ITER, initial_v(t, p))
+            f = lambda v: get_volume(p, t, v, beta_value, gamma_value, delta_value)
+            f_prime = lambda v: get_volume(p, t, v, beta_value, gamma_value, delta_value)
+            volume = newton.calculate(f,f_prime)
+            print("O VOLUME ENCONTRADO FOI " + str(volume))
+
+main()
