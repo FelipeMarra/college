44b14b692e5da0216262c515ab95d3cfb29ffb20
Felipe Marra <felipeferreiramarra@gmail.com>
2021-03-14 14:00:33 -0300

    Update problem.py

--- roots/activity_1/problem.py
+++ roots/activity_1/problem.py
@@ -18,11 +18,7 @@ AZINHO = 0.01855
 BZINHO = -0.01587
 CZINHO = 12.83 * 10 ** 4
 
-
-def initial_v(t, p):
-    return RZAO*t/p
-
-
+# equasion parameters
 def beta(t):
     return (RZAO * t * B_ZERO) - A_ZERO - (RZAO * CZINHO/t**2)
 
@@ -34,15 +30,19 @@ def gamma(t):
 def delta(t):
     return (RZAO * B_ZERO * BZINHO * CZINHO) / t ** 2
 
-
+# the equasion
 def get_equasion(p, t, v, beta, gamma, delta):
     return (RZAO * t * v ** -1) + (beta * v ** -2) + (gamma * v ** -3) + (delta * v ** -4) - p
 
-
+# its derivative
 def get_equasion_prime(p, t, v, beta, gamma, delta):
     return ((RZAO * t) * (-v ** -2)) + (beta * (-2 * v ** -3)) + (gamma * (-3 * v ** -4)) + (delta * (-4 * v ** -5))
 
+# V0
+def initial_v(t, p):
+    return RZAO*t/p
 
+# z
 def get_compressiblility(p, v, t):
     return (p*v) / (RZAO*t)
 
@@ -68,11 +68,9 @@ def main():
             print("PARA PRESSAO = " + str(p))
             newton = NewtonsMethod(EPSILON, MAX_ITER, initial_v(t, p))
 
-            def f(v): return get_equasion(
-                p, t, v, beta_value, gamma_value, delta_value)
+            f = lambda v: get_equasion(p, t, v, beta_value, gamma_value, delta_value)
 
-            def f_prime(v): return get_equasion_prime(
-                p, t, v, beta_value, gamma_value, delta_value)
+            f_prime = lambda v: get_equasion_prime(p, t, v, beta_value, gamma_value, delta_value)
 
             volume = newton.calculate(f, f_prime)
 
