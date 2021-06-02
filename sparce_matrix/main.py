8bb3daafad24a23b2d49c1a46e12dd64554d92f4
Felipe Marra <felipeferreiramarra@gmail.com>
2021-06-02 19:56:22 -0300

    summ & remove & density & degree

--- sparce_matrix/main.py
+++ sparce_matrix/main.py
@@ -3,32 +3,50 @@ from sparce_matrix import SparceMatrix
 graph = SparceMatrix()
 
 
-def insert():
+def insert_vertex():
     origin = int(input("origin: "))
     destiny = int(input("destiny: "))
     weight = int(input("weight: "))
 
-    graph.insert(origin, destiny, weight)
+    graph.insert_vertex(origin, destiny, weight)
+
+
+def remove_vertex():
+    origin = int(input("origin: "))
+    destiny = int(input("destiny: "))
+    graph.remove_vertex(origin, destiny)
 
 
 def summary_print():
+    print("vextexes:",graph.num_vertexes())
+    print("edges:",graph.num_edges())
+    print("density:",graph.density())
     print(graph.edge_map)
 
+def vertex_degree():
+    origin = int(input("origin: "))
+    print("vertex degree:",graph.vertex_degree(origin))
 
 # menu
 option = 0
 while option != -1:
     print()
     print("-1 exit")
-    print("0 insert an edge")
-    print("1 summary or print")
+    print("0 insert an vertex")
+    print("1 remove an vertex")
+    print("2 summary & print")
+    print("3 vertex degree")
     print()
 
     option = int(input("select an option: "))
 
     if option == 0:
-        insert()
+        insert_vertex()
     elif option == 1:
+        remove_vertex()
+    elif option == 2:
         summary_print()
+    elif option == 3:
+        vertex_degree()
     else:
         print("pay attention dummy!!!")
--- sparce_matrix/sparce_matrix.py
+++ sparce_matrix/sparce_matrix.py
@@ -1,5 +1,5 @@
-# an entry in the edge_map for each edge with a data_map for the desntiny
-# and weight
+# an entry in the edge_map for each edge with a data_map for the vertexe's
+# desntiny and weight
 class SparceMatrix:
 
     def __init__(self):
@@ -11,14 +11,53 @@ class SparceMatrix:
             "weight": weight
         }
 
-    def insert(self, origin: int, destiny: int, weight: float):
+    def insert_vertex(self, origin: int, destiny: int, weight: float):
         if origin < 0:
             print("origin < 0")
             return
 
         data = self.data_map(destiny, weight)
 
+        #create edges
         if not origin in self.edge_map:
             self.edge_map[origin] = {}
+        
+        if not destiny in self.edge_map:
+            self.edge_map[destiny] = {}
 
+        #create vertex
         self.edge_map[origin][destiny] = data
+
+    def remove_vertex(self, origin: int, destiny: int):
+        if not origin in self.edge_map:
+            print("this origin is not there")
+            return
+        
+        if not destiny in self.edge_map[origin]:
+            print("this origin is not there")
+            return
+        
+        self.edge_map[origin].pop(destiny)
+
+        #remove edge case it's empty
+        if not self.edge_map[origin]:
+            self.edge_map.pop(origin)
+
+    def num_vertexes(self):
+        summ = 0
+        for origin in self.edge_map:
+            summ += len(self.edge_map[origin])
+        return summ
+    
+    def num_edges(self):
+        return len(self.edge_map)
+
+    def density(self):
+        edg = self.num_edges()
+        return self.num_vertexes()/(edg*(edg-1))
+
+    def vertex_degree(self, origin: int):
+        if not origin in self.edge_map:
+            print("this origin is not there")
+            return
+        return len(self.edge_map[origin])
