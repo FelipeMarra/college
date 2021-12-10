396e3104f15518ab31f75bc33984904c787e5f53
Fabrício Dinali <dinalifabricio@yahoo.com>
2021-12-10 09:43:35 -0300

    final

--- neural_network/main.py
+++ neural_network/main.py
@@ -21,10 +21,16 @@ def main():
         option = int(input("Option: "))
 
         if option == 0:
-            sentence = str(input("Your sentence: "))
-            test_sentece(sentence, tokenizer)
-            continue
+            #In this version of the code the tokenizer state during training is not saved
+            if tokenizer == None:
+                print("\n Run the option 1 first \n")
+                continue
 
+            sentence = str(input("Your sentence: "))
+            prob, sentiment = test_sentece(sentence, tokenizer)
+            print("\n PROBABILIDADE = ", prob, "SENTIMENTO = ", sentiment, "\n")
+            continue
+        
         if option == 1:
             tokenizer = train()
             continue
--- neural_network/nn/predict.py
+++ neural_network/nn/predict.py
@@ -6,11 +6,11 @@ from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-#TODO: dar um jeito no tokenizer
 def test_sentece(sentence, tokenizer):
-    
     model = keras.models.load_model("sentiment_model")
     sequence = tokenizer.texts_to_sequences([sentence])
     padded = pad_sequences(sequence, padding='post', maxlen=280)
-    print(model.predict(padded))
-    #TODO: fazer o retorno certo
+    prob = model.predict(padded)
+    sentiment = "positive" if prob > 0.5 else "negative"
+    
+    return prob, sentiment
